a07fcb6aa90aec9d9d5e07faeb174b88dd0d8733
ilya_goose <ilyabogatyryov638@gmail.com>
2024-04-09 21:17:45 +0300

    Создал бота

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('d')
+from aiogram import Bot, Dispatcher
+import asyncio
+
+bot = Bot(token='6910912601:AAGuk-ug_YZZ412jjMoW1RXHbftapThUrHY')
+dp = Dispatcher()
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
