2bc5333223b66a8260e81117464e0e2f7b6552f0
ilya_goose <ilyabogatyryov638@gmail.com>
2024-04-09 21:37:54 +0300

    Сделал приветствие для бота

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 import asyncio
 
 bot = Bot(token='6910912601:AAGuk-ug_YZZ412jjMoW1RXHbftapThUrHY')
 dp = Dispatcher()
+
+murkub = ReplyKeyboardMarkup(
+    keyboard=[[KeyboardButton(text='📝 Последние новости 📝'), KeyboardButton(text='🎭 Трансферы 🎭')]],
+    resize_keyboard=True)
+
+
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer(
+        f'👋 Здравствуте, {message.from_user.last_name}!\nЯ помогу вам узнать много интересного о ФУТБОЛЕ ⚽\n😇 Пожалуйста, выберите интересующую категорию:',
+        reply_markup=murkub)
+
 
 async def main():
     await dp.start_polling(bot)
